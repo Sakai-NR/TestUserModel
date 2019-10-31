4b042d87e04266c537e939cf6bbc605c56011c10
Noriaki Sakai <avengers3000ily@gmail.com>
2019-10-31 23:26:45 +0900

    addFile

---
+++ TestUserModel1/TestUserModel1/TopViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TopViewController.swift
+//  TestUserModel1
+//
+//  Created by 酒井典昭 on 2019/10/31.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
